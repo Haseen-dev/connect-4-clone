0465d2193017feb1f72799e51c9df120b2bb47b8
Haseen-dev <mhmhaseen1998@gmail.com>
2025-03-02 12:02:00 +0530

    add dynamic styling

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,38 +2,36 @@ import React from "react";
 import GameCircle from "./GameCircle";
 
 const GameBoard = () => {
-  const style={
+  const style = {
     display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr 1fr",
-        gridTemplateRows: "1fr 1fr 1fr 1fr",
-        padding:"20px",
-        width:"500px",
-        // margin:"0 auto"
-        position:"absolute",
-        top:"25%",
-        left:"50%",
-        marginLeft:"-250px"
-  }
+    gridTemplateColumns: "1fr 1fr 1fr 1fr",
+    gridTemplateRows: "1fr 1fr 1fr 1fr",
+    padding: "20px",
+    width: "500px",
+    // margin:"0 auto"
+    position: "absolute",
+    top: "25%",
+    left: "50%",
+    marginLeft: "-250px",
+  };
   return (
-    <div
-      style={style}
-    >
-      <GameCircle id={1} name="red" />
-      <GameCircle id={2} name="blue" />
-      <GameCircle id={3} name="red" />
-      <GameCircle id={4} name="blue" />
-      <GameCircle id={5} name="red" />
-      <GameCircle id={6} name="blue" />
-      <GameCircle id={7} name="red" />
-      <GameCircle id={8} name="blue" />
-      <GameCircle id={9} name="red" />
-      <GameCircle id={10} name="blue" />
-      <GameCircle id={11} name="red" />
-      <GameCircle id={12} name="blue" />
-      <GameCircle id={13} name="red" />
-      <GameCircle id={14} name="blue" />
-      <GameCircle id={15} name="red" />
-      <GameCircle id={16} name="blue" />
+    <div style={style}>
+      <GameCircle id={1} />
+      <GameCircle id={2} />
+      <GameCircle id={3} />
+      <GameCircle id={4} />
+      <GameCircle id={5} />
+      <GameCircle id={6} />
+      <GameCircle id={7} />
+      <GameCircle id={8} />
+      <GameCircle id={9} />
+      <GameCircle id={10} />
+      <GameCircle id={11} />
+      <GameCircle id={12} />
+      <GameCircle id={13} />
+      <GameCircle id={14} />
+      <GameCircle id={15} />
+      <GameCircle id={16} />
     </div>
   );
 };
--- src/components/GameCircle.jsx
+++ src/components/GameCircle.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../Game.css"
 
 const onclick = (id) => {
   alert("on click" + id);
@@ -7,20 +8,26 @@ const onclick = (id) => {
 const GameCircle = ({ id, name }) => {
   const style={
     color: name === "red" ? "red" : "blue",
-    backgroundColor: name === "red" ? "red" : "blue",
+    //backgroundColor: name === "red" ? "red" : "blue",
     height:"100px",
     width:"100px",
     margin:"10px",
     borderRadius:"50%"
   }
-  console.log(id);
   return (
-    <div
-      onClick={() => onclick(id)}
-      style={style}
-    >
-      {name}
-    </div>
+    // <div
+    //   onClick={() => onclick(id)}
+    //   style={{...style,backgroundColor:id%2===0 ? "blue":"red"}}
+    // >
+    //   {name}
+    // </div>
+     <div
+     className={`${id%2===0?"even":"odd"}`}
+     onClick={() => onclick(id)}
+     style={style}
+   >
+     {name}
+   </div>
   );
 };
 
