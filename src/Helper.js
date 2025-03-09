cd619a0cfcf476d2c2053e77f27593777d1d21ff
Haseen-dev <mhmhaseen1998@gmail.com>
2025-03-09 14:26:39 +0530

    Draw bug fixed

--- src/Helper.js
+++ src/Helper.js
@@ -31,7 +31,7 @@ export const isWinner = (gameBoard, currentMove, currentPlayer) => {
 export const isDraw = (gameBoard, currentMove, currentPlayer) => {
   let board = [...gameBoard];
   board[currentMove] = currentPlayer;
-  let count = board.reduce((n, x) => n + (x === 0));
+  let count = board.reduce((n, x) => n + (x === 0),0);
   console.log(`count ${count}`);
   return count === 0;
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "../Game.css";
-import { GAME_STATE_WIN, GAME_STATE_PLAYING } from "../Constant";
+import {
+  GAME_STATE_WIN,
+  GAME_STATE_PLAYING,
+  GAME_STATE_DRAW,
+} from "../Constant";
 
-const Header = ({ gameState, currentPlayer,winPlayer }) => {
+const Header = ({ gameState, currentPlayer, winPlayer }) => {
   const renderLabel = () => {
     switch (gameState) {
       case GAME_STATE_PLAYING:
         return <div>Player {currentPlayer} Turn</div>;
       case GAME_STATE_WIN:
         return <div>Player {winPlayer} wins</div>;
+      case GAME_STATE_DRAW:
+        return <div>Game is a Draw</div>;
+
+      default:
     }
   };
   return (
