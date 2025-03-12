548a0e1e81394dda18d05cc1f4e10c26f415c86e
Haseen-dev <mhmhaseen1998@gmail.com>
2025-03-12 23:33:06 +0530

    footer button gets bugs2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Footer = (onClickEvent) => {
+const Footer = ({onClickEvent}) => {
   return (
     <div className='panel footer'>
         <button onClick={onClickEvent}>New Game</button>
