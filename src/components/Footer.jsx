c49faeb3e3689bc4e04032379a3e45c0daa8deb3
Haseen-dev <mhmhaseen1998@gmail.com>
2025-03-13 14:31:27 +0530

    Suggest button created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = ({onClickEvent}) => {
   return (
     <div className='panel footer'>
         <button onClick={onClickEvent}>New Game</button>
+        <button>Suggest</button>
     </div>
   )
 }
